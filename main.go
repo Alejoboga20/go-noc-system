ab9ef5aba10c996844f4bca754ea574853051858
alejoboga-io <alejo.boga@pendo.io>
2024-10-08 14:25:34 -0300

    create check use case

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/Alejoboga20/go-noc-system/domain/usecases"
+	"github.com/Alejoboga20/go-noc-system/infrastructure"
 )
 
+func success() {
+	fmt.Println("Success")
+}
+
+func failure() {
+	fmt.Println("Failure")
+}
+
 func main() {
-	fmt.Println("NoC System")
+	httpClient := infrastructure.NewHTTPClientImplementation()
+	checkService := usecases.NewCheckServiceImplementation(
+		httpClient,
+		nil,
+		success,
+		failure)
+
+	checkService.Check("https://www.google.com/")
 }
